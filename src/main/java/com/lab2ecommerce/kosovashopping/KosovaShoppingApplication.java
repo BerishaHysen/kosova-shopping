cafdff1fbd9206a43bb76068e3c90e4585ab0ba3
BerishaHysen <hb46326@ubt-uni.net>
2022-05-23 02:30:36 +0200

    spring-starter-files

---
+++ src/main/java/com/lab2ecommerce/kosovashopping/KosovaShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.lab2ecommerce.kosovashopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosovaShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosovaShoppingApplication.class, args);
+	}
+
+}
