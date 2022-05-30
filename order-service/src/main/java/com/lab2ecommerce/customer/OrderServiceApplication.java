58be8c04b1d13daa271694b8bb65bb4a50b9cec4
BerishaHysen <hb46326@ubt-uni.net>
2022-05-30 19:30:09 +0200

    bug fixing at order-serice eureka-client

--- order-service/src/main/java/com/lab2ecommerce/customer/OrderServiceApplication.java
+++ order-service/src/main/java/com/lab2ecommerce/customer/OrderServiceApplication.java
@@ -3,8 +3,10 @@ package com.lab2ecommerce.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
